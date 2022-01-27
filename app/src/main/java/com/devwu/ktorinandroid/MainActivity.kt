677c5a0b6bae857665dfab88cdf45ebb2771f0f3
Wu Nan <geek5nan@gmail.com>
2022-01-27 14:10:29 +0800

    Use Ktor-1.6.7

--- app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
+++ app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
@@ -2,15 +2,15 @@ package com.devwu.ktorinandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.ktor.server.application.call
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 import org.slf4j.LoggerFactory
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // ERROR: java.lang.NoClassDefFoundError: Failed resolution of: Ljava/lang/management/ManagementFactory;
 //        embeddedServer(Netty, applicationEngineEnvironment {
 //            parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
 //            log = LoggerFactory.getLogger("ktor.application")
@@ -27,12 +26,11 @@ class MainActivity : AppCompatActivity() {
 //            }
 //            module {
 //                routing {
-//                    get("/") { call.respondText { "Hello World" } }
+//                    get("/") { call.respondText { "Hello Netty" } }
 //                }
 //            }
 //        }).start(false)
 
-        // ERROR: call no respond
         embeddedServer(CIO, applicationEngineEnvironment {
             parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
             log = LoggerFactory.getLogger("ktor.application")
@@ -41,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             }
             module {
                 routing {
-                    get("/") { call.respondText { "Hello World" } }
+                    get("/") { call.respondText { "Hello CIO" } }
                 }
             }
         }).start(false)
