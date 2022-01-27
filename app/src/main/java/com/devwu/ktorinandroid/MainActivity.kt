7166a674de73941074bf676d1a921dbd166c3901
Wu Nan <geek5nan@gmail.com>
2022-01-27 14:01:49 +0800

    init repo

---
+++ app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.devwu.ktorinandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.ktor.server.application.call
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import org.slf4j.LoggerFactory
+import kotlin.coroutines.EmptyCoroutineContext
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ERROR: java.lang.NoClassDefFoundError: Failed resolution of: Ljava/lang/management/ManagementFactory;
+//        embeddedServer(Netty, applicationEngineEnvironment {
+//            parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
+//            log = LoggerFactory.getLogger("ktor.application")
+//            connector {
+//                port = 8080
+//            }
+//            module {
+//                routing {
+//                    get("/") { call.respondText { "Hello World" } }
+//                }
+//            }
+//        }).start(false)
+
+        // Error
+        embeddedServer(CIO, applicationEngineEnvironment {
+            parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
+            log = LoggerFactory.getLogger("ktor.application")
+            connector {
+                port = 8080
+            }
+            module {
+                routing {
+                    get("/") { call.respondText { "Hello World" } }
+                }
+            }
+        }).start(true)
+    }
+}
