9059b183f966f04a739897c820ba3310c8d73996
Wu Nan <geek5nan@gmail.com>
2022-04-09 21:14:51 +0800

    Testing Netty engine with SSL ability

--- app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
+++ app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
@@ -3,23 +3,48 @@ package com.devwu.ktorinandroid
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.ktor.server.application.call
-import io.ktor.server.cio.CIO
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.sslConnector
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import org.slf4j.LoggerFactory
+import java.security.KeyStore
 import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val keyStore = KeyStore.getInstance("PKCS12")
+        keyStore.load(assets.open("keystore.jks"),"qwe123".toCharArray())
         // ERROR: java.lang.NoClassDefFoundError: Failed resolution of: Ljava/lang/management/ManagementFactory;
-//        embeddedServer(Netty, applicationEngineEnvironment {
+        embeddedServer(Netty, applicationEngineEnvironment {
+            parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
+            log = LoggerFactory.getLogger("ktor.application")
+            connector {
+                port = 8080
+            }
+            sslConnector(
+                keyStore,
+                keyAlias = "samplealias",
+                keyStorePassword = { "qwe123".toCharArray() },
+                privateKeyPassword = { "qwe123".toCharArray() }
+            ) {
+                port = 8443
+            }
+            module {
+                routing {
+                    get("/") { call.respondText { "Hello World" } }
+                }
+            }
+        }).start(false)
+
+        // ERROR: call no respond
+//        embeddedServer(CIO, applicationEngineEnvironment {
 //            parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
 //            log = LoggerFactory.getLogger("ktor.application")
 //            connector {
@@ -31,19 +56,5 @@ class MainActivity : AppCompatActivity() {
 //                }
 //            }
 //        }).start(false)
-
-        // ERROR: call no respond
-        embeddedServer(CIO, applicationEngineEnvironment {
-            parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
-            log = LoggerFactory.getLogger("ktor.application")
-            connector {
-                port = 8080
-            }
-            module {
-                routing {
-                    get("/") { call.respondText { "Hello World" } }
-                }
-            }
-        }).start(false)
     }
 }
