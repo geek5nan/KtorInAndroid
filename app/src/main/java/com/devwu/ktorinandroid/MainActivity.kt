220614a3d8715adeada18ea151eeff8a15a37194
Wu Nan <geek5nan@gmail.com>
2022-04-09 21:21:19 +0800

    Add Exception descriptions.

--- app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
+++ app/src/main/java/com/devwu/ktorinandroid/MainActivity.kt
@@ -19,8 +19,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val keyStore = KeyStore.getInstance("PKCS12")
-        keyStore.load(assets.open("keystore.jks"),"qwe123".toCharArray())
+        val keyStore = KeyStore.getInstance("PKCS12").apply {
+            load(assets.open("keystore.jks"), "qwe123".toCharArray())
+        }
+        // Exception:
+        // 1. java.lang.NoSuchMethodException: sun.misc.Unsafe.copyMemory [class java.lang.Object, long, class java.lang.Object, long, long]
+        // 2. No implementation found for int io.netty.channel.kqueue.Native.sizeofKEvent()
+        // 3. No implementation found for int io.netty.channel.epoll.Native.offsetofEpollData()
+        // 4. java.lang.IllegalArgumentException: Failed to load any of the given libraries: [netty_tcnative_linux_aarch_64, netty_tcnative_linux_aarch_64_fedora, netty_tcnative_aarch_64, netty_tcnative]
+        // 5. No implementation found for int io.netty.internal.tcnative.Library.aprMajorVersion() (tried Java_io_netty_internal_tcnative_Library_aprMajorVersion and Java_io_netty_internal_tcnative_Library_aprMajorVersion__)
+        // 6. Failed to initialize netty-tcnative; OpenSslEngine will be unavailable. See https://netty.io/wiki/forked-tomcat-native.html for more information.
+        // 7. java.lang.ClassNotFoundException: java.lang.management.ManagementFactory. Could not invoke ManagementFactory.getRuntimeMXBean().getName(); Android?
+        // 8. Failed to get SOMAXCONN from sysctl and file /proc/sys/net/core/somaxconn. Default: 128
         embeddedServer(Netty, applicationEngineEnvironment {
             parentCoroutineContext = EmptyCoroutineContext + parentCoroutineContext
             log = LoggerFactory.getLogger("ktor.application")
